70fea37587cded3d456812c74efe3c909977fc2b
iamankandaas <das.99.ankan@gmail.com>
2025-07-22 17:56:59 +0530

    CICD run - 5

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     version='0.1.0',
     description='ete mlops',
     author='Ankan Das',
+    author_email="das.99.ankan@gmail.com", # Replace with your email
     license='',
+    install_requires=[]  # We keep this empty as we install from requirements.txt
+
 )
